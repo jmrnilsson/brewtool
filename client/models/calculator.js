74a876cd15eda5eb13bd31c2314d90d826437ad9
martin nilsson <martin.jr.nilsson@gmail.com>
2015-10-04 16:35:33 +0200

    Shortend computeds

--- client/models/calculator.js
+++ client/models/calculator.js
@@ -9,32 +9,21 @@ define([
     var originalGravity = ko.observable('1050');
     var finalGravity = ko.observable('1010');
     var mode = ko.observable('compensated');
+    var modeText = ko.pureComputed(function(){return mode().charAt(0).toUpperCase() + mode().slice(1);});
     var alcoholByVolume = ko.observable();
-
-    var alcoholByVolumeText = ko.computed(function(){
-        var abv = alcoholByVolume();
-        if (abv == undefined || isNaN(abv)){
-            return 'N/A';
-        }
-        return abv + ' %';
-    });
-    
-    var modeText = ko.computed(function(){
-        var modeValue = mode();
-        return modeValue.charAt(0).toUpperCase() + modeValue.slice(1);
-    });
+    var alcoholByVolumeText = ko.pureComputed(function(){ return alcoholByVolume() ?  alcoholByVolume() + ' %' : 'N/A'});
 
     function calculate(data, event){
         var model = new AbvCalculator(originalGravity(), finalGravity());
         model.alcoholByVolume(mode(), 
-            function(abv){
-                alcoholByVolume(abv.toFixed(2));
-                events.emit('calculated-abv', {abv: abv, mode: mode()});
-            },    
-            function(error){
-                toastr.error(error);
-                alcoholByVolume(null);
-            });
+        function(abv){
+            alcoholByVolume(abv.toFixed(2));
+            events.emit('calculated-abv', {abv: abv, mode: mode()});
+        },    
+        function(error){
+            toastr.error(error);
+            alcoholByVolume(null);
+        });
     };
 
     var CalculatorViewModel = function(){
