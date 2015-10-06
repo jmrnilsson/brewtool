8f9ec109321bef61495a74aedea36afe5e996c73
martin nilsson <martin.jr.nilsson@gmail.com>
2015-10-06 23:41:04 +0200

    Updated glyphicons for calc. added es6

--- client/utils/extensions.js
+++ client/utils/extensions.js
@@ -5,14 +5,11 @@ define([
 	
   function gravityExtension(target, args) {
       target.hasError = ko.observable();
-      target.hasSuccess = ko.observable();
       
       function validate(value) {
         var errors = value == undefined || isNaN(value) || value < 1 || value > 1.3 && value < 1000 || value > 1300;
           target.hasError(errors);
-          target.hasSuccess(!errors);
-      }
-      
+      }      
       validate(target());
       target.subscribe(validate);
       return target;
