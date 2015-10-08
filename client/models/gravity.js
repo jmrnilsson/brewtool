579280aa4f5cad967f3536cb0b30da00e13edbf3
Martin Nilsson <martin.jr.nilsson@gmail.com>
2015-10-08 20:11:41 +0200

    added helper func to Gravity

--- client/models/gravity.js
+++ client/models/gravity.js
@@ -3,15 +3,13 @@ define([
 ], function (ko) {
 'use strict';
 	
+    function convert(gravity){
+        return gravity > 1000 ? gravity / 1000 : gravity;
+    }
+    
     var original = ko.observable().extend({gravity: null});
     var final = ko.observable().extend({gravity: null});
-    var hasError = ko.pureComputed(function(){
-        var og = original();
-        var fg = final();
-        og = og > 1000 ? og / 1000 : og;
-        fg = fg > 1000 ? fg / 1000 : fg;
-        return og < fg;
-    });
+    var hasError = ko.pureComputed(function(){return convert(original()) < convert(final());});
 
     var Gravity = function(originalGravity, finalGravity){
         var self = this;
