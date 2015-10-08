69e1e00cc5e57c6a800324f34d7929b98e637624
Martin Nilsson <martin.jr.nilsson@gmail.com>
2015-10-08 21:47:02 +0200

    Removed unnessary documentation

--- arduino/arduino_temperature_sensing.ino
+++ arduino/arduino_temperature_sensing.ino
@@ -8,36 +8,33 @@ DallasTemperature sensors(&oneWire);
 // Assign the addresses of your 1-Wire temp sensors. See the tutorial on how to obtain these addresses:
 // http://www.hacktronics.com/Tutorials/arduino-1-wire-address-finder.html
 
-DeviceAddress insideThermometer = { 0x28, 0x7A, 0x33, 0x49, 0x05, 0x00, 0x00, 0xC5 };
+DeviceAddress thermometer = { 0x28, 0x7A, 0x33, 0x49, 0x05, 0x00, 0x00, 0xC5 };
 
 void setup(void)
 {
-  // Start serial port
   Serial.begin(9600);
-  // Start up the library
   sensors.begin();
-  // Set the resolution to 10 bit (good enough?)
   sensors.setResolution(insideThermometer, 10);
 }
 
 void printTemperature(DeviceAddress deviceAddress)
 {
-  float tempC = sensors.getTempC(deviceAddress);
-  if (tempC == -127.00) 
+  float celcius = sensors.getTempC(deviceAddress);
+  if (celcius == -127.00) 
   {
     Serial.print("Error getting temperature");
   }
   else
   {
-    Serial.print(tempC);
+    Serial.print(celcius);
   }
+  Serial.print("\n\r");
 }
 
 void loop(void)
 { 
   delay(100);
   sensors.requestTemperatures();
-  printTemperature(insideThermometer);
-  Serial.print("\n\r");
+  printTemperature(thermometer);
 }
 
--- server/frun.cmd
+++ server/frun.cmd
@@ -1,7 +1,7 @@
 @echo off
 rem Add intiger arg for sensing simulation. To start, approximately 1000 ms (= 1 Hz) per 
 rem normal operation. For performance benchmarks this could decreased. Though,
-rem most temperature sensor can not provide accurate readings at lower than 4 Hz (250 
+rem most temperature sensor can not provide accurate readings at lower than 40 Hz (25 
 rem ms) at a time. So the is little point in going any lower. 
 node fdaemon.js
 echo.
