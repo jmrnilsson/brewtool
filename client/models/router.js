faa5e6ad0ccfc55d75a8cfbaf2665f9679b445a0
Martin Nilsson <martin.jr.nilsson@gmail.com>
2015-10-08 23:15:42 +0200

    Added some comments

--- client/models/router.js
+++ client/models/router.js
@@ -6,8 +6,9 @@ define([
 'use strict';
 
   var bowerPackage = JSON.parse(bower);
-  var routes = ['temperature', 'calculator', 'log'];
-  var route = ko.observable('temperature');
+  var defaultRoute = 'temperature';
+  var routes = [defaultRoute, 'calculator', 'log'];
+  var route = ko.observable(defaultRoute);
   
   routes.forEach(function(r){
     Path.map('#/' + r).to(function () {route(r);});
