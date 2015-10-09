89b1e9eec0246b9b8ecef14bda3dbf452af9eb32
Martin Nilsson <martin.jr.nilsson@gmail.com>
2015-10-09 08:53:31 +0200

    added router work

--- client/models/router.js
+++ client/models/router.js
@@ -9,15 +9,24 @@ define([
   var defaultRoute = 'temperature';
   var routes = [defaultRoute, 'calculator', 'log'];
   var route = ko.observable(defaultRoute);
+  var combinedRoutes = [];
+  
+  function Route(routeText){
+    var self = this;
+    self.href = '#/' + routeText;
+    self.value = routeText;
+    self.text = routeText.charAt(0).toUpperCase() + routeText.slice(1);
+  }
   
   routes.forEach(function(r){
     Path.map('#/' + r).to(function () {route(r);});
+    combinedRoutes.push(new Route(r));
   });
   Path.root('#/' + route());
     
   return {
     route: route,
-    routes: routes,
+    routes: combinedRoutes,
     version: bowerPackage.version,
     listen: Path.listen
   };
