a81079a90ec8f0d5fc4e2cc5246a5668bf60f17f
Martin Nilsson <martin.jr.nilsson@gmail.com>
2015-10-12 22:17:21 +0200

    started preparing for chartjs

--- client/utils/framework.js
+++ client/utils/framework.js
@@ -12,28 +12,28 @@ define([
 		socket.on('sense-temperature', function (event) {emit('sense-temperature', event);});
 	}
 	
-	function emit(topic, data){		
-		if (topic == undefined){
-            throw "Event is missing a topic";
-        }
-		
-		events.unshift({
-			topic: topic, 
-			created: new Date().toUTCString(),
-			data: data
-		});
-		if(events().length > 10000){
-			events.pop();
+	function emit(topic, data){
+		function add(topic, data){		
+			if (topic == undefined){
+				throw new Error("Event is missing a topic");
+			}
+			
+			events.unshift({
+				topic: topic, 
+				created: new Date().toUTCString(),
+				data: data
+			});
+			if(events().length > 10000){
+				events.pop();
+			}
 		}
-	}
-	
-	function emitAsync(topic, data){
-		setTimeout(emit(topic, data), 0);
+		
+		setTimeout(add(topic, data), 0);
 	}
 
 	return {
 		events: events, 
-		emit: emitAsync, 
+		emit: emit, 
 		attach: attach
 	};		
 });
