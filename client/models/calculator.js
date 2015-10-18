4aa5bd4a1324dc9d28c8d6875cd5c4554bf030d9
Martin Nilsson <martin.jr.nilsson@gmail.com>
2015-10-18 22:56:36 +0200

    Preparing to synchronize calculator

--- client/models/calculator.js
+++ client/models/calculator.js
@@ -10,42 +10,47 @@ define([
     var gravity = new Gravity(1050, 1010);
     var mode = ko.observable('compensated');
     var modeText = ko.pureComputed(function(){return mode().charAt(0).toUpperCase() + mode().slice(1);});
-    var alcoholByVolume = ko.observable();
-    // Perhaps this shouldn't be computed. It is determined on very specific occasions i.e. when 'calculate' is called. Rather have
-    // a function that formats to the correct output when setting. However both abv and formatted abv needs to provided by this 
-    // view model.
-    var alcoholByVolumeText = ko.pureComputed(function(){ return alcoholByVolume() ?  alcoholByVolume() + ' %' : 'N/A'});
+    var abv = ko.observable();
 
     function calculate(data, event){
-        var requested = arguments.length > 1;
+        var onclick = arguments.length > 1;
         var model = new Abv(gravity.original(), gravity.final());
-        model.alcoholByVolume(mode(), 
-        function(abv){
-            alcoholByVolume(abv.toFixed(2));
-            // Make pretty after Q.promise has been merged from ES6 branch or replaced with synchronous call 
-            // (but then validation needs to be incorporated into calculator.js or gravity.js)
-            if (requested) {
-                events.emit('calculated-abv', {abv: abv, mode: mode()});
-            }
-        },    
-        function(error){
-            toastr.error(error);
-            alcoholByVolume(null);
-        });
+        model.alcoholByVolume(mode(), success(onclick), fail);
     };
-    
+
+    function format(alcoholByVolume){
+        return alcoholByVolume ? alcoholByVolume + ' %' : 'N/A';
+    }
+
+    function fail(failure){
+        abv(format(null));
+        failure.errors.forEach(function(error){
+            toastr.error(error);
+        });
+    }
+
+    function success(onclick){
+        function setAbv(alcoholByVolume){
+            abv(format(alcoholByVolume.toFixed(2)));
+            if (onclick) {
+                events.emit('calculated-abv', {abv: alcoholByVolume, mode: mode()});
+            }
+        }
+        return setAbv;
+    }
+
     var CalculatorViewModel = function(){
         var self = this;
 
         // observables
         self.gravity = gravity;
-        self.alcoholByVolumeText = alcoholByVolumeText;
+        self.abv = abv;
         self.modeText = modeText;
 
         // actions
-        self.calculate = calculate;     
+        self.calculate = calculate;
         self.mode = mode;
-        
+
         self.calculate();
     };
 
