5cace9764ad939b72bef8f162aaff57a9a91c066
Martin Nilsson <martin.jr.nilsson@gmail.com>
2015-10-18 10:09:03 +0200

    Removed complicated routing and route templates

--- client/app.js
+++ client/app.js
@@ -3,19 +3,38 @@ define([
   'utils/framework',
   'jquery',
   'utils/extensions',
-  'views/routeView',
   'path',
   'immutable',
-  'text!./bower.json'
-], function(ko, events, $, extensions, routeView, Path, Immutable, bowerConfiguration){
+  'text!./bower.json',
+  'utils/guid'
+], function(ko, events, $, extensions, Path, Immutable, bowerConfiguration, guid){
+
+  function fadeIn(html, parentElement){
+    var identities = [];
+    $(html).each(function(index, row){
+            var id = guid.newGuid();
+            row.style.display = 'none';
+            row.id = id;
+            identities.push(id);
+            $(parentElement).append(row);
+    });
+
+    var delay = 100;
+    identities.forEach(function(id){
+      var el = $('#'+id);
+      el.fadeIn(delay += 100, function(){
+        // Clean up temporary id's once fade is completed
+        el.removeAttr('id');
+      });
+    });
+  }
 
   function start(){
 
     // Declare routes and read configuration
     var bower = JSON.parse(bowerConfiguration);
-    var defaultRoute = 'temperature';
-    var routes = Immutable.List.of(defaultRoute, 'calculator', 'log');
-    var model = {route: ko.observable(defaultRoute)};
+    var routes = Immutable.List.of('temperature', 'calculator', 'log');
+    var model = {route: ko.observable(routes.get(0))};
 
     // Register ko
     ko.components.register('temperature', { require: 'views/temperatureView' });
@@ -36,10 +55,8 @@ define([
     events.attach();
 
     $(function(){
-      var navbar = document.getElementById('navbar-collapse');
       var navbarText = document.getElementsByClassName('navbar-text')[0];
       navbarText.innerHTML = bower.version;
-      routeView.create(routes, navbar.firstElementChild);
       ko.applyBindings(model, document.body);
     });
   }
