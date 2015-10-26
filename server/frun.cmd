e212ca7834ef9a015a363e34a73a8ec0cead9631
Martin Nilsson <martin.jr.nilsson@gmail.com>
2015-10-26 21:28:36 +0100

    Moved deamon to root

--- server/frun.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Add intiger arg for sensing simulation. To start, approximately 1000 ms (= 1 Hz) per 
-rem normal operation. For performance benchmarks this could decreased. Though,
-rem most temperature sensor can not provide accurate readings at lower than 40 Hz (25 
-rem ms) at a time. So the is little point in going any lower. 
-node fdaemon.js
-echo.
-pause
-cls
