6ba63cc625b2436ee030204a5e5c8448e82514ea
Martin Nilsson <martin.jr.nilsson@gmail.com>
2015-10-31 16:03:36 +0100

    Added sqlite and gitignore for db/sqlite files

---
+++ db.js
@@ -0,0 +1,42 @@
+var sqlite3 = require('sqlite3').verbose();
+var fs = require("fs");
+var file = "log.db";
+var db;
+
+
+function init(){
+  if(!fs.existsSync(file))
+  {
+    db = new sqlite3.Database(file);
+    db.serialize(function() {
+      db.run('CREATE TABLE tGravity (session TEXT, timestamp INTEGER, originalgravity REAL, finalgravity REAL)');
+      db.run('CREATE TABLE tSense (session TEXT, timestamp INTEGER, celsius REAL)');
+    });
+  } else{
+    db = new sqlite3.Database(file);
+  }
+}
+
+function addGravity(data){
+  var stmt = db.prepare('INSERT INTO tGravity VALUES (?, ?, ?, ?)');
+  stmt.run(data.session, data.timestamp, data.originalGravity, data.finalGravity);
+  stmt.finalize();
+}
+
+function addSense(data){
+  var stmt = db.prepare('INSERT INTO tSense VALUES (?, ?, ?)');
+  stmt.run(data.session, data.timestamp, data.celsius);
+  stmt.finalize();
+}
+
+function noop(){}
+exports.init = init;
+exports.close = db ? db.close() : noop;
+exports.gravities = {
+  'add': addGravity
+  //,'get': getGravity
+};
+exports.senses = {
+  'add': addSense
+  //,'get': getSense
+};
--- fdaemon.js
+++ fdaemon.js
@@ -2,9 +2,12 @@
 var express = require('express')
   , app = module.exports = express()
   , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+  , io = require('socket.io').listen(server)
+  , db = require('./db.js');
 
+db.init();
 var configurationPort = 3000;
+var session = newGuid();
 
 app.use(express.static('./client/'));
 server.listen(configurationPort);
@@ -12,33 +15,43 @@ console.log('Fake daemon is listening on port ' + configurationPort + '.\nPress 
 
 var push = function (temperature) {
   setTimeout(function() {
-    io.sockets.emit('sense-temperature', {
-        temperature: temperature,
-        utc: new Date().toUTCString()
-      });
+    var sense = {session: session, timestamp: new Date().toUTCString(), celsius: temperature}
+    //   stmt.run(data.session, data.timestamp, data.celsius);
+    db.senses.add(sense);
+    //io.sockets.emit('sense-temperature', sense);
+    io.sockets.emit('sense-temperature', {temperature: temperature, utc: new Date().toUTCString()});
     push(Math.floor(Math.random() * 3) - 1 + temperature);
   }, 2000);
 }
 
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
-
-db.serialize(function() {
-  db.run("CREATE TABLE tGravity (session TEXT, timestamp INTEGER, originalgravity REAL, finalgravity REAL)");
-  db.run("CREATE TABLE tSense (session TEXT, timestamp INTEGER, celsius REAL)");
-
-  var stmt = db.prepare("INSERT INTO tGravity VALUES (?, ?, ?, ?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run('a'+1, 11000, 1.5, 1.2 + 0.1 * i);
+function newGuid() {
+  function seed() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
   }
-  stmt.finalize();
+  return seed() + seed() + '-' + seed() + '-' + seed() + '-' + seed() + '-' + seed() + seed() + seed();
+}
 
-  db.each("SELECT rowid AS id, session, timestamp, originalgravity, finalgravity FROM tGravity", function(err, row) {
-    console.log(err);
-      console.log(row.id + ": " + row.session + ", " + row.timestamp + ", " + row.originalgravity + ", " + row.finalgravity);
-  });
-});
-
-db.close();
+// var sqlite3 = require('sqlite3').verbose();
+// var db = new sqlite3.Database(':memory:');
+//
+// db.serialize(function() {
+//   db.run("CREATE TABLE tGravity (session TEXT, timestamp INTEGER, originalgravity REAL, finalgravity REAL)");
+//   db.run("CREATE TABLE tSense (session TEXT, timestamp INTEGER, celsius REAL)");
+//
+//   var stmt = db.prepare("INSERT INTO tGravity VALUES (?, ?, ?, ?)");
+//   for (var i = 0; i < 10; i++) {
+//       stmt.run('a'+1, 11000, 1.5, 1.2 + 0.1 * i);
+//   }
+//   stmt.finalize();
+//
+//   db.each("SELECT rowid AS id, session, timestamp, originalgravity, finalgravity FROM tGravity", function(err, row) {
+//     console.log(err);
+//       console.log(row.id + ": " + row.session + ", " + row.timestamp + ", " + row.originalgravity + ", " + row.finalgravity);
+//   });
+// });
+//
+// db.close();
 
 push(Math.floor((Math.random() * 100) + 1));
