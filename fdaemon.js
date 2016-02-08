f169662c5d1e2484d845ba57670ca8eb3e8990ac
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-02-08 19:50:07 +0100

    use named functions instead

--- fdaemon.js
+++ fdaemon.js
@@ -1,8 +1,8 @@
 
-var express = require('express')
-  , app = module.exports = express()
-  , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 var configurationPort = 3000;
 
@@ -10,14 +10,21 @@ app.use(express.static('./client/'));
 server.listen(configurationPort);
 console.log('Fake daemon is listening on port ' + configurationPort + '.\nPress ctrl + c to close.');
 
-var push = function (temperature) {
-  setTimeout(function() {
-    io.sockets.emit('sense-temperature', {
+var first = Math.floor((Math.random() * 100) + 1)
+emit(first);
+
+function emit(temperature){
+    var current = {
         temperature: temperature,
         utc: new Date().toUTCString()
-      });
-    push(Math.floor(Math.random() * 3) - 1 + temperature);
-  }, 2000);
+    }
+    var next = Math.floor(Math.random() * 3) - 1 + temperature;
+
+    function send(){
+        io.sockets.emit('sense-temperature', current);
+        emit(next);
+    }
+
+  setTimeout(send, 2000);
 }
 
-push(Math.floor((Math.random() * 100) + 1));
