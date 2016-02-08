facbb41762faa7c4d9113e86fc7a396b2097ec13
Martin Nilsson <martin.jr.nilsson@gmail.com>
2016-02-08 19:33:53 +0100

    rem comment

--- client/models/alarm.js
+++ client/models/alarm.js
@@ -24,7 +24,6 @@ define([
 
 	function evaluate(){
 		var temp = sense().data.celsius;
-		// Allow floats as it's quite possible to be super-specific
 		var below = parseFloat(low()) > temp;
 		var above = parseFloat(high()) < temp;
 		var play = played == undefined || (new Date().getTime() - played) > 10000;
